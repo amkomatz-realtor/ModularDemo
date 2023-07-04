0bf2175a56eefb137a16720b08065eb97009a4c8
PC Nguyen <pc.nguyen@realtor.com>
2023-07-03 20:22:13 -0700

    enable feature flag

--- ModularDemo/Router.swift
+++ ModularDemo/Router.swift
@@ -3,15 +3,19 @@ import RDCBusiness
 import RDCSearch
 import RDCHomes
 import RDCFeed
+import RDCHomesV2
 
 class Router: SearchRouting, FeedRouting {
     private let resolver: AppResolver
+    private let isV2Enabled: Bool = false
     
     init(resolver: AppResolver) {
         self.resolver = resolver
     }
     
     func getDestination(forListingId id: UUID) -> AnyView {
-        AnyView(ListingDetailView(id: id, resolver: resolver))
+        return isV2Enabled
+            ? AnyView(EmptyView())
+            : AnyView(ListingDetailView(id: id, resolver: resolver))
     }
 }
