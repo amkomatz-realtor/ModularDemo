e5de88027ae36546ce6ad03877009c6ba9adcab0
PC Nguyen <pc.nguyen@realtor.com>
2023-07-05 16:31:24 -0700

    setting up unit test data for sections.

---
+++ RDCHomesV2/Tests/RDCHomesV2Tests/Util/ListingDetailSection+Test.swift
@@ -0,0 +1,36 @@
+import Foundation
+@testable import RDCHomesV2
+
+extension ListingDetail.ForRentSection {
+    var listingHero: ListingHero? {
+        if case let .listingHero(listingHero, _) = self {
+            return listingHero
+        }
+        
+        return nil
+    }
+    
+    var listingStatus: (text: String, price: String, address: ListingAddress)? {
+        if case let .listingStatus(text, price, address, _) = self {
+            return (text, price, address)
+        }
+        
+        return nil
+    }
+    
+    var listingSize: ListingSize? {
+        if case let .listingSize(listingSize, _) = self {
+            return listingSize
+        }
+        
+        return nil
+    }
+    
+    var neighborhood: Neighborhood? {
+        if case let .neighborhood(neighborhood, _) = self {
+            return neighborhood.latestValue.loadedView
+        }
+        
+        return nil
+    }
+}
