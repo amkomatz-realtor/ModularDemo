e82b9f9a0287fc9e373b7958418ac76d9e9c4259
PC Nguyen <pc.nguyen@realtor.com>
2023-07-08 11:51:15 -0700

    adding tool to facilitate state change side effect

--- RDCCore/Sources/RDCCore/Binding/BaseViewModel.swift
+++ RDCCore/Sources/RDCCore/Binding/BaseViewModel.swift
@@ -17,8 +17,8 @@ open class BaseViewModel<DataView>: ObservableObject, IHashIdentifiable {
         self.uuid = .hashableUUID
     }
     
-    /// Updating the latest value using `async await`
-    @MainActor public func publish(_ value: DataView) {
+    /// Manually update the value, use this together with the `observable` factory method
+    public func publish(_ value: DataView) {
         self.dataView = value
     }
     
@@ -54,7 +54,14 @@ public extension BaseViewModel {
     
     /// Providing a generic ViewModel with a single `dataView` value
     /// Useful for preview canvas
-    static func single<T>(_ value: T) -> BaseViewModel<T> {
+    static func single<DataView>(_ value: DataView) -> BaseViewModel<DataView> {
+        .init(value)
+    }
+    
+    /// Providing a generic ViewModel to update the `dataView` over time
+    /// The name difference between this and `single` is only for readability sake (at least for now)
+    /// Useful when we have a side effect that need to update the state within the same view.
+    static func observable(initial value: DataView) -> BaseViewModel<DataView> {
         .init(value)
     }
 }
