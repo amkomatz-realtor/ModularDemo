6de523925d5fdeb7391cb44093ba7dc6b56b7c9a
PC Nguyen <pc.nguyen@realtor.com>
2023-07-09 14:50:08 -0700

    fine tuning instruction data model

--- RDCHomesV2/Sources/RDCHomesV2/Code Walkthrough/Model/InstructionDataModel.swift
+++ RDCHomesV2/Sources/RDCHomesV2/Code Walkthrough/Model/InstructionDataModel.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 struct InstructionDataModel: Equatable, Decodable {
-    let message: String
-    let imageName: String
+    let designingSwiftUIView: String
+    let mappingDataInViewModel: String
+    let usage: String
 }
 
 #if targetEnvironment(simulator)
 
 extension InstructionDataModel {
     init() {
-        self.message = "Replaced me with proper datamodel"
-        self.imageName = "chevron.right.circle"
+        self.designingSwiftUIView = "Adding data needed to the SwiftUI struct, then layout the view in the body extension"
+        self.mappingDataInViewModel = "In the ViewModel file, mapping the data model to the data that this view need using the private extension"
+        self.usage = "Anywhere you need to create this view, create it using ComponentViewModel(_:).dataView"
     }
 }
 
