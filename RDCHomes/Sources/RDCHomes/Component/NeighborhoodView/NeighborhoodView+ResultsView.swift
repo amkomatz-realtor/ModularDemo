8fc797e9fa28f1ebe35c12b4152977f1477b6b4b
amkomatz-realtor <austin-michael.komatz@realtor.com>
2023-07-10 14:25:47 -0400

    Add viewmodel for neighborhood results view

--- RDCHomes/Sources/RDCHomes/Component/NeighborhoodView/NeighborhoodView+ResultsView.swift
+++ RDCHomes/Sources/RDCHomes/Component/NeighborhoodView/NeighborhoodView+ResultsView.swift
@@ -3,36 +3,45 @@ import RDCCore
 
 extension NeighborhoodView {
     struct ResultsView: View {
-        let name: String
-        let rating: String
+        @StateObject private var viewModel: ViewModel
         
-        init(name: String, rating: Double) {
-            self.name = name
-            
-            let formatter = NumberFormatter()
-            formatter.maximumFractionDigits = 1
-            self.rating = "\(formatter.string(from: rating as NSNumber)!)/10"
-        }
-        
-        init(neighborhood: NeighborhoodModel) {
-            self.init(name: neighborhood.name, rating: neighborhood.rating)
+        init(_ viewModel: ViewModel) {
+            _viewModel = StateObject(wrappedValue: viewModel)
         }
         
         var body: some View {
             VStack(alignment: .leading, spacing: 8) {
-                Text("Neighborhood")
+                Text(viewModel.title)
                     .font(.caption.bold())
                     .foregroundColor(.gray)
                     .identify(.homes.neighborhoodTitle)
                 
                 HStack {
-                    Text(name)
+                    Text(viewModel.name)
                         .identify(.homes.neighborhoodName)
-                    Text(rating)
+                    Text(viewModel.rating)
                         .identify(.homes.neighborhoodRating)
                 }
                 .font(.callout)
             }
+        }
+    }
+}
+
+
+extension NeighborhoodView.ResultsView {
+    class ViewModel: ObservableObject {
+        let title: String
+        let name: String
+        let rating: String
+        
+        init(_ neighborhood: NeighborhoodModel) {
+            title = "Neighborhood"
+            name = neighborhood.name
+            
+            let formatter = NumberFormatter()
+            formatter.maximumFractionDigits = 1
+            self.rating = "\(formatter.string(from: neighborhood.rating as NSNumber)!)/10"
         }
     }
 }
