fd9b28e40df67b1845359aefc787401301ce5353
PC Nguyen <pc.nguyen@realtor.com>
2023-07-12 06:33:14 -0700

    adding extension for test helper

--- RDCHomesV2/Tests/RDCHomesV2Tests/Util/LazyDataView+Test.swift
+++ RDCHomesV2/Tests/RDCHomesV2Tests/Util/LazyDataView+Test.swift
@@ -27,3 +27,29 @@ extension LazyDataView {
         return nil
     }
 }
+
+extension LazyViewModel {
+    var isViewHidden: Bool {
+        if case .hidden = self.dataView {
+            return true
+        }
+        
+        return false
+    }
+    
+    var loadingView: (any IHashIdentifiableView)? {
+        if case .loading(let view) = self.dataView {
+            return view
+        }
+        
+        return nil
+    }
+    
+    var loadedDataView: DataView? {
+        if case .loaded(let dataView) = self.dataView {
+            return dataView
+        }
+        
+        return nil
+    }
+}
