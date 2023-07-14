9a8be9bcc5aca5cfa3d6f02629a56a35d565f909
PC Nguyen <pc.nguyen@realtor.com>
2023-07-14 08:10:39 -0700

    matching side effect test

---
+++ RDCHomesV2/Tests/RDCHomesV2Tests/Tests/Component/SeeMoreLinkViewModelTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import CustomDump
+@testable import RDCHomesV2
+
+final class SeeMoreLinkViewModelTests: XCTestCase {
+
+    var sut: SeeMoreLinkViewModel!
+
+    func testItGeneratesViewFromModel() {
+        givenViewModel(with: .previewForSaleListingModel(), router: StubHostRouter())
+        
+        XCTAssertEqual(sut.dataView.displayText, "See more details")
+    }
+    
+    func testItRoutesToProperPlace() {
+        let router = StubHostRouter()
+        let detailModel: DetailListingModel = .previewForSaleListingModel()
+        givenViewModel(with: detailModel, router: router)
+        
+        // when
+        sut.dataView.onTap.occurs()
+        
+        // then
+        XCTAssertEqual(router.verifiedDestination?.contains("listingAdditionalDetails(id: \(detailModel.id)"), true)
+    }
+
+    // MARK: - Test Helpers
+    
+    /// Guide: Replace `InstructionDataModel` with the actual data model(s) that provide data for your view.
+    func givenViewModel(with model: DetailListingModel, router: StubHostRouter) {
+        let stubResolvers = StubHomesResolver()
+        stubResolvers.stubHostRouter = router
+        sut = SeeMoreLinkViewModel(listingModel: model, resolver: stubResolvers)
+    }
+}
